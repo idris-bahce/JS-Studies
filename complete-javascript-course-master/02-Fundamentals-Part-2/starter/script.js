5191406107af819a24e3a4ed021699eba415bb6b
idris <idrisbahce26@gmail.com>
2023-05-26 09:58:46 +0300

    work out

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,21 +1,33 @@
 "use strict";
-dolpSCore1 = 44;
-dolpSCore2 = 23;
-dolpSCore3 = 71;
+// dolpSCore1 = 44;
+// dolpSCore2 = 23;
+// dolpSCore3 = 71;
 
-coalaSCore1 = 65;
-coalaSCore2 = 54;
-coalaSCore3 = 49;
+// coalaSCore1 = 65;
+// coalaSCore2 = 54;
+// coalaSCore3 = 49;
 
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
 
-function checkWinner(avgColalas, avgDolphins) {
-    if (avgColalas > avgDolphins) {
-        console.log(`Winner is ${avgColalas}!`)
-    } else if (avgDolphins > avgColalas) {
-        console.log(`Winner is ${avgDolphins}!`)
-    } else {
-        console.log("It is a Draw!")
+// function checkWinner(avgColalas, avgDolphins) {
+//     if (avgColalas > avgDolphins) {
+//         console.log(`Winner is ${avgColalas}!`)
+//     } else if (avgDolphins > avgColalas) {
+//         console.log(`Winner is ${avgDolphins}!`)
+//     } else {
+//         console.log("It is a Draw!")
+//     }
+// }
+
+const idris = {
+    firstName: "idris",
+    lastName: "bahce",
+    birthYear: 1996,
+    job: "sdet",
+    caculateAge: function (birthYear) {
+        return 2023 - birthYear;
     }
-}
+};
+
+console.log(idris.caculateAge(idris.birthYear));
