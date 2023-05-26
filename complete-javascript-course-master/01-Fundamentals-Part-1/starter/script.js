16ee0c6467a90c1d33c79db6fda5c8317e38928d
idris <idrisbahce26@gmail.com>
2023-05-26 05:58:02 +0300

    work out

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,5 @@
+var receip = 40;
+var tip;
+50 <= receip <= 300 ? tip = receip * 0.15 : tip = receip * 0.2;
+
+console.log(tip)
---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,15 @@
+"use strict";
+dolpSCore1 = 44;
+dolpSCore2 = 23;
+dolpSCore3 = 71;
+
+coalaSCore1 = 65;
+coalaSCore2 = 54;
+coalaSCore3 = 49;
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+
+function checkWinner(average1, average2) {
+
+}
