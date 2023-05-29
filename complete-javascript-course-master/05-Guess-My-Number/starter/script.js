4b2610e43e37aae802fb707909a550abc2b5a55a
idris <idrisbahce26@gmail.com>
2023-05-29 09:22:02 +0300

    work out

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,3 +1,5 @@
 'use strict';
 
 document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 12;
+document.querySelector('.score').textContent = 10;
