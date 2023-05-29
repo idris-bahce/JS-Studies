1cc2ed62a2018a8547007ee6f8a13603c2d6bb71
idris <idrisbahce26@gmail.com>
2023-05-29 09:25:41 +0300

    work out

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -3,3 +3,7 @@
 document.querySelector('.message').textContent = 'Correct Number!';
 document.querySelector('.number').textContent = 12;
 document.querySelector('.score').textContent = 10;
+//For input tag we use value not textContent
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 12;
+console.log(document.querySelector('.guess').value);
