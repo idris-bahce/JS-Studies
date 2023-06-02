0105efcc286c77f965990cff33356515c9df6ac7
idris <idrisbahce26@gmail.com>
2023-06-02 11:24:45 +0300

    work out

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 document.querySelector('.message').textContent = 'Correct Number!';
 document.querySelector('.number').textContent = 12;
 document.querySelector('.score').textContent = 10;
@@ -7,3 +7,11 @@ document.querySelector('.score').textContent = 10;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 12;
 console.log(document.querySelector('.guess').value);
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
