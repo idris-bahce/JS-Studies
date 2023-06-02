06921b08828ca9024d2d2728afffa35b1fbad205
idris <idrisbahce26@gmail.com>
2023-06-02 11:38:57 +0300

    work out

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -8,10 +8,25 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 12;
 console.log(document.querySelector('.guess').value);
 */
+
+var number = Math.floor(Math.random() * 20) + 1;
+var score = document.querySelector('.score').textContent;
+console.log(number);
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (number < guess) {
+    document.querySelector('.message').textContent = '☝ Higer...';
+    document.querySelector('.score').textContent = --score;
+  } else if (number > guess) {
+    document.querySelector('.message').textContent = '👇 Lower...';
+    document.querySelector('.score').textContent = --score;
+  } else {
+    document.querySelector('.message').textContent = '👍 You got it!';
+    if (document.querySelector('.highscore').textContent < score) {
+      document.querySelector('.highscore').textContent = score;
+    }
   }
 });
