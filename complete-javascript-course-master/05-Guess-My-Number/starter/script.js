a0e67bbe8ea648d7dcba39526df9e595810883c7
idris <idrisbahce26@gmail.com>
2023-06-05 20:43:16 +0300

    work out

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -18,13 +18,25 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
   } else if (number < guess) {
-    document.querySelector('.message').textContent = '☝ too high...';
-    document.querySelector('.score').textContent = --score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '☝ too high...';
+      document.querySelector('.score').textContent = --score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (number > guess) {
-    document.querySelector('.message').textContent = '👇 too low...';
-    document.querySelector('.score').textContent = --score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '👇 too low...';
+      document.querySelector('.score').textContent = --score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else {
     document.querySelector('.message').textContent = '👍 You got it!';
+    document.querySelector('.number').textContent = number;
+    document.querySelector('body').style.backgroundColor = 'green';
     if (document.querySelector('.highscore').textContent < score) {
       document.querySelector('.highscore').textContent = score;
     }
