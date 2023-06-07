3543ddf1b91cd7f69c172e5003c518e038e43bef
idris <idrisbahce26@gmail.com>
2023-06-07 13:43:15 +0300

    work out

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -3,4 +3,18 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelector('.show-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', function () {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
